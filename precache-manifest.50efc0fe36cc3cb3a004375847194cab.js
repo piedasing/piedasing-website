a6e66b6abb8574768936645a0eb954627408dbed
pieda <pieda@coder.com.tw>
2019-07-07 14:19:20 +0800

    Updates

---
+++ precache-manifest.50efc0fe36cc3cb3a004375847194cab.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a33c24dc8b37cb4d35c",
+    "url": "js/f2e.f1e1cdda.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "cbf3b4422440b3119836",
+    "url": "css/work.70d7de7c.css"
+  },
+  {
+    "revision": "c70c91259fa384bbe9e7",
+    "url": "css/app.57c0055d.css"
+  },
+  {
+    "revision": "fd68fa227b2dbef59cce",
+    "url": "css/chunk-vendors.472c2b9a.css"
+  },
+  {
+    "revision": "fd68fa227b2dbef59cce",
+    "url": "js/chunk-vendors.f1e1cdda.js"
+  },
+  {
+    "revision": "4a33c24dc8b37cb4d35c",
+    "url": "css/f2e.d6564734.css"
+  },
+  {
+    "revision": "ce98d8883e15b46e48a7",
+    "url": "js/about.f1e1cdda.js"
+  },
+  {
+    "revision": "c70c91259fa384bbe9e7",
+    "url": "js/app.f1e1cdda.js"
+  },
+  {
+    "revision": "cbf3b4422440b3119836",
+    "url": "js/work.f1e1cdda.js"
+  },
+  {
+    "revision": "baada0220811802f9dfd53f0e7656ec0",
+    "url": "img/profile.baada022.jpg"
+  },
+  {
+    "revision": "2956a4a77ef07427c0b9d5b0c9623728",
+    "url": "img/coder.2956a4a7.png"
+  },
+  {
+    "revision": "c3ade398c945ed52cba8fc2532bb640e",
+    "url": "img/logo-bg.c3ade398.png"
+  },
+  {
+    "revision": "8f561bb601329c9b0d5d17b9ae73129c",
+    "url": "index.html"
+  },
+  {
+    "revision": "ce98d8883e15b46e48a7",
+    "url": "css/about.d5976c07.css"
+  }
+];
